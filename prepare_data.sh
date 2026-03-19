dad4140c90be818c519b10c42a79354b3d3c57a9
breadbread1984 <breadbread1984@163.com>
2026-03-19 10:54:16 +0800

    update

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,47 +1,4 @@
 #!/bin/bash
-# recent_pdb_rsync.sh - 2021-09后到2024年底mmCIF (ay→cz)
-
-OUTDIR="./recent_pdb_2021_2024"
-mkdir -p "$OUTDIR"
-
-# ========== 2021 Q4 ==========
-rsync -rlpt -v -z --delete --port=33444 \
-  rsync.rcsb.org::ftp_data/structures/divided/mmCIF/ay/ \
-  "$OUTDIR/ay/"
-
-# ========== 2022 全年 (az→bz) ==========
-for dir in az ba bb bc bd be bf bg bh bi bj bk bl bm bn bo bp bq br bs bt bu bv bw bx by bz; do
-  echo "Syncing 2022: $dir..."
-  rsync -rlpt -v -z --delete --port=33444 \
-    "rsync.rcsb.org::ftp_data/structures/divided/mmCIF/$dir/" \
-    "$OUTDIR/$dir/" &
-done
-wait
-
-# ========== 2023 全年 (ca→cz) ==========
-for dir in ca cb cc cd ce cf cg ch ci cj ck cl cm cn co cp cq cr cs ct cu cv cw cx cy cz; do
-  echo "Syncing 2023: $dir..."
-  rsync -rlpt -v -z --delete --port=33444 \
-    "rsync.rcsb.org::ftp_data/structures/divided/mmCIF/$dir/" \
-    "$OUTDIR/$dir/" &
-done
-wait
-
-# ========== 2024 全年 (da→dz，但截断到dz前) ==========
-for dir in da db dc dd de df dg dh di dj dk dl dm dn do dp dq dr ds dt du dv dw dx dy dz; do
-  echo "Syncing 2024: $dir..."
-  rsync -rlpt -v -z --delete --port=33444 \
-    "rsync.rcsb.org::ftp_data/structures/divided/mmCIF/$dir/" \
-    "$OUTDIR/$dir/" &
-done
-wait
-
-echo "✅ 同步完成！2021-2024 PDB数据在 $OUTDIR/"
-echo "总计目录：$(ls -d $OUTDIR/* | wc -l)"
-
-find "$OUTDIR" -name '*.cif.gz' -exec gunzip {} \;
-
-find "$OUTDIR" -name *.cif | tr ' ' '\n' > sample_list.txt
-wget https://cdn.rcsb.org/resources/sequence/clusters/clusters-by-entity-40.txt
-python3 scripts/gen_ccd_cache.py -c ccd_cache_dir -n 4
+wget https://af3-dev.tos-cn-beijing.volces.com/release_data.tar.gz
+tar -xzvf release_data.tar.gz && rm release_data.tar.gz
 python3 scripts/prepare_training_data.py -i sample_list.txt -o preprocessed_indices.csv -b preprocessed_dataset -c clusters-by-entity-40.txt -n 4
