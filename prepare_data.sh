3a1c715d5903138d168d31b856b4608c6f17a3cd
breadbread1984 <breadbread1984@163.com>
2026-03-20 09:47:44 +0800

    update

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 bash scripts/database/download_pretenix_data.sh --full
-python3 scripts/prepare_training_data.py -i sample_list.txt -o preprocessed_indices.csv -b preprocessed_dataset -c clusters-by-entity-40.txt -n 4
+
+python3 scripts/prepare_training_data.py \
+  -i mmcif \
+  -o indices/weightedPDB_indices_before_2021-09-30_wo_posebusters_resolution_below_9.csv.gz \
+  -b mmcif_bioassembly \
+  -c common/clusters-by-entity-40.txt \
+  -n 64 \
+  -d
