1358230c636fc63066735799b1a25d7bf2bab9d2
breadbread1984 <breadbread1984@163.com>
2026-03-25 08:11:56 +0800

    update

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export PROTENIX_ROOT_DIR=/home/xieyi/release_data
 bash scripts/database/download_pretenix_data.sh --full
+
+"""
 python3 scripts/prepare_training_data.py \
   -i mmcif \
   -o indices/weightedPDB_indices_before_2021-09-30_wo_posebusters_resolution_below_9.csv.gz \
@@ -8,3 +10,4 @@ python3 scripts/prepare_training_data.py \
   -c common/clusters-by-entity-40.txt \
   -n 64 \
   -d
+"""
