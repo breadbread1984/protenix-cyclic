1de150afe0187840de753b22355173a7740a0c1b
breadbread1984 <breadbread1984@163.com>
2026-04-27 09:31:32 +0800

    update

---
+++ finetuning_cmds/eval.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+torchrun --nproc_per_node=4 runner/train.py \
+	--model_name protenix_base_default_v1.0.0 \
+	--max_steps 0 \
+	--load_checkpoint_path <to be filled> \
+	--load_ema_checkpoint_path <to be filled> \
+	--data.test_sets recentPDB_1536_sample384_0925,posebusters_0925 \
+        --data.train_sets "" \
+	--base_dir ./eval_output \
+	--run_name eval_only \
+	--dtype bf16 \
+	--eval_ema_only true
