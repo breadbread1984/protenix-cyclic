e5b02c1a7816d99cf771270df9f0dca860e3ed9f
breadbread1984 <breadbread1984@163.com>
2026-04-27 09:37:45 +0800

    update

--- finetuning_cmds/eval.sh
+++ finetuning_cmds/eval.sh
@@ -6,7 +6,8 @@ torchrun --nproc_per_node=4 runner/train.py \
 	--load_checkpoint_path <to be filled> \
 	--load_ema_checkpoint_path <to be filled> \
 	--data.test_sets recentPDB_1536_sample384_0925,posebusters_0925 \
-        --data.train_sets "" \
+        --data.train_sets weightedPDB_before2109_wopb_nometalc_0925 \
+        --data.weightedPDB_before2109_wopb_nometalc_0925.base_info.pdb_list filename_list.txt \
 	--base_dir ./eval_output \
 	--run_name eval_only \
 	--dtype bf16 \
