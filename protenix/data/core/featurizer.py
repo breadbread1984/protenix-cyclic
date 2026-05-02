0e4f5817bf7cef35f15352242c5534bcea48807b
breadbread1984 <breadbread1984@163.com>
2026-05-02 10:43:38 +0800

    update

--- protenix/data/core/featurizer.py
+++ protenix/data/core/featurizer.py
@@ -526,7 +526,7 @@ class Featurizer(object):
         bond_token_j = atom_idx_to_token_idx[bond_array[:,1]]
         full_adj_matrix[bond_token_i, bond_token_j] = 1
         full_adj_matrix[bond_token_j, bond_token_i] = 1
-        bond_features = {'adj': torch.Tensor(full_adj_matrix)}
+        bond_features.update({'adj': torch.Tensor(full_adj_matrix)})
         return bond_features
 
     def get_extra_features(self) -> dict[str, torch.Tensor]:
